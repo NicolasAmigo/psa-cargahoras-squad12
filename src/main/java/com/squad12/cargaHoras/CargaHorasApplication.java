d6a8e97f69da7a08360ec2a9c939cf30128cd11e
NicolasAmigo <namigo@fi.uba.ar>
2023-06-14 14:30:16 -0300

    Copy Springboot Template

---
+++ src/main/java/com/squad12/cargaHoras/CargaHorasApplication.java
@@ -0,0 +1,13 @@
+package com.squad12.cargaHoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squad12/cargaHoras/CargaHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad12.cargaHoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
