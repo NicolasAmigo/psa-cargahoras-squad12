273804541dcfa847d8557f471ae55efe116e16fa
NicolasAmigo <namigo@fi.uba.ar>
2023-06-24 14:21:43 -0300

    Succesfully conects to database

--- src/main/java/com/squad12/cargaHoras/Model/Horas.java
+++ src/main/java/com/squad12/cargaHoras/Model/Horas.java
@@ -1,12 +1,10 @@
 package com.squad12.cargaHoras.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "horas")
 public class Horas {
 
     @Id
