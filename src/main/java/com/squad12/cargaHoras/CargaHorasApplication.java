d345154b827b8ba0c163a1feee07437ef2721f8a
NicolasAmigo <namigo@fi.uba.ar>
2023-06-27 11:26:28 -0300

    Added swagger docs

--- src/main/java/com/squad12/cargaHoras/CargaHorasApplication.java
+++ src/main/java/com/squad12/cargaHoras/CargaHorasApplication.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @SpringBootApplication
